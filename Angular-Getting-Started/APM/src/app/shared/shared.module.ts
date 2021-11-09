f2e1df312eea5d91ab81f114f7eb3e53b3e05c55
Ben Cayton <benjamin_cayton@tcenergy.com>
2021-11-08 19:53:21 -0500

    Finished modules section of course

---
+++ Angular-Getting-Started/APM/src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './convert-to-spaces.pipe';
+import { StarComponent } from './star.component';
+
+@NgModule({
+  declarations: [
+    StarComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FormsModule,
+    CommonModule,
+    StarComponent,
+    ConvertToSpacesPipe
+  ]
+})
+export class SharedModule { }
